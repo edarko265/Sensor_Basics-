5d0a7ed71b7bd812172bc3f7cf72719c42ca79b4
vincent <vincentbui2108@gmail.com>
2023-11-15 10:59:59 +0200

    Eric function for sensor and speaker done

--- door_sensor.py
+++ door_sensor.py
@@ -1,24 +1,47 @@
 import RPi.GPIO as GPIO
 import time
-
-def magnectic(door_sensor_pin):
+from Speaker import play_buzzer
+def magnectic(door_sensor_pin=17, speaker_pin=18):
+    # Set up GPIO
     GPIO.setmode(GPIO.BCM)
+
+    # GPIO input for magnectic
     GPIO.setup(door_sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    wtime = 0.5
+
+    # GPIO output for speaker
+    GPIO.setup(speaker_pin, GPIO.OUT)
 
     try:
         while True:
-            door_state = GPIO.input(door_sensor_pin)
-            if door_state == GPIO.LOW:  # closed
-                return False
-            else:  # opened
-                return True
-            time.sleep(wtime)  
+            # Read the state of magnectic
+            door_switch_state = GPIO.input(door_sensor_pin)
+
+            if door_switch_state == GPIO.LOW:
+                # door switch is open, turn on the speaker
+                GPIO.output(speaker_pin, GPIO.LOW)
+                #return False
+                print("door switch is closed - Speaker OFF")
+                
+            else:
+                # door switch is closed, turn off the speaker
+                #return True
+                GPIO.output(speaker_pin, GPIO.HIGH)
+                print("door switch is opened - Speaker ON")
+                play_buzzer()
+               
+            time.sleep(0.5)  # Adds a small delay to avoid rapid change of state
 
     except KeyboardInterrupt:
-        pass
-
-    finally:
+        # Clean up GPIO on keyboard interrupt CTRL+C
+        print("Keyboard interrupt detected. Cleaning up GPIO.")
         GPIO.cleanup()
+        print("Exiting the program.")
 
-# door_sensor_pin = 17
+# Uncomment the following line if you want to run this script independently
+#magnectic()
+
+#code to call it in another file
+# from door_sensor import magnectic
+
+# Call the function with the desired GPIO pins
+#magnectic(door_sensor_pin=17, speaker_pin=18)
