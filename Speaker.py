65819063c1a1032ec588a3222a816b402ce9eafd
eric <eric.darko@edu.savonia.fi>
2023-11-18 12:42:51 +0200

    ryme sound added

--- Speaker.py
+++ Speaker.py
@@ -1,20 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 import pygame
+import random
 
+#list of MP3 file paths
+ryme_voices = ["sound1.mp3", "sound4.mp3"]
 pygame.mixer.init()
-buzzer = pygame.mixer.Sound("alarm.mp3")
+buzzer = None
 
 def playsound():
+    
+    random_file = random.choice(ryme_voices)
+    buzzer = pygame.mixer.Sound(random_file)
+
+    # Play the sound
     playing = buzzer.play()
+
     while playing.get_busy():
         pygame.time.delay(100)
 
 def stop_playsound():
+    # Check if the sound is currently playing
     playing = buzzer.play()
+
     if playing.get_busy():
         playing.stop()
-
+        
 #playsound()
 #time.sleep(5) 
 #stop_playsound()
